02bf11581023a178682709fc0a0b657ad0ae16a2
Dave Fravel <dave@bluegear.io>
2019-08-20 07:53:09 -0400

    styling unordered list

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -26,7 +26,7 @@
         @endforeach
     @endif
 
-    <div class="border-b border-blue-200 mb-10 pb-4" v-pre>
+    <div class="border-b border-blue-200 mb-10 pb-4 blog-post" v-pre>
         @yield('content')
     </div>
 
